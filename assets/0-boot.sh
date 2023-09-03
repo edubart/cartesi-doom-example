eb0e977405d1169cd0f821b8559102b472da129e
Eduardo Bart <edub4rt@gmail.com>
2023-09-03 14:30:04 -0300

    Add -nomenu option

--- assets/0-boot.sh
+++ assets/0-boot.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-exec ./doom -iwad doom1.wad "$@"
+if [ $# -eq 0 ]; then # no arguments
+    exec ./doom \
+        -iwad doom1.wad \
+        -skill 3 -warp 1 1 \
+        -nomenu \
+        -autoquit \
+        "$@"
+else
+    exec ./doom \
+        -iwad doom1.wad \
+        "$@"
+fi
